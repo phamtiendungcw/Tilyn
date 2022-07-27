bf3781d7dc87bcc28bbd7ee6e070081e958500eb
Phạm Tiến Dũng <56197293+phamtiendungcwit@users.noreply.github.com>
2022-07-27 16:10:51 +0700

    Add ApplicationDbContext

---
+++ TilynWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TilynWeb.Models;
+
+namespace TilynWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
